0d51700125c35c92926d9673d66e3669fe35d8ac
eftekher-alam <eftekher@yahoo.com>
2022-12-27 22:12:47 +0600

    Multi-statement table-valued function

--- DDL/FunctionDefinition.sql
+++ DDL/FunctionDefinition.sql
@@ -23,4 +23,40 @@ FROM authors a
 
 
 
+----->> Tag wise all Information (Multi-statement table-valued function) <<-------
+	--This function will return a table which table will assign from multiple table
 
+CREATE OR ALTER FUNCTION fn_TagWiseBooks
+(
+	@tag NVARCHAR(100)			-- Parameter 
+)
+RETURNS @OutTable TABLE			-- Output table
+		(
+			tag NVARCHAR(100) ,
+			title NVARCHAR(100),
+			price MONEY,
+			totalBookOfTheTag INT
+		)
+AS
+
+BEGIN
+	INSERT INTO @OutTable
+		SELECT t.tag, b.title, b.coverPrice, 0 -- Value assigned into the table from multiple statement 
+		FROM tags t
+		INNER JOIN booksTags bt ON bt.tagId = t.tagId
+		INNER JOIN books b ON b.bookId = bt.bookId
+		WHERE LOWER(t.tag) LIKE '%'+LOWER(@tag)+'%'
+	UPDATE @OutTable			-- Value assigned into the table from multiple statement 
+	SET totalBookOfTheTag = (
+						SELECT COUNT(*)
+						FROM tags t
+						INNER JOIN booksTags bt ON bt.tagId = t.tagId
+						WHERE LOWER(t.tag) LIKE '%'+LOWER(@tag)+'%'
+					)
+	RETURN;   -- RETURN key word will be placed at the last.
+END
+
+
+SELECT * FROM booksTags
+SELECT * FROM tags
+SELECT * FROM dbo.fn_TagWiseBooks('Programming')
